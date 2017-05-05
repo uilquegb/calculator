2bec44fcc0d47bc114133bebd6dda25f88db2924
Uilque Messias <uilquegb@hotmail.com>
2017-05-05 18:28:24 -0300

    Calculate the number and show it on display container

--- src/Calculator.js
+++ src/Calculator.js
@@ -13,10 +13,20 @@ const inputButtons = [
 
 class Calculator extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: 0
+        };
+    }
+
     render() {
         return (
             <View style={Style.rootContainer}>
-                <View style={Style.displayContainer}></View>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
                 <View style={Style.inputContainer}>
                     {this._renderInputButtons()}
                 </View>
@@ -49,7 +59,18 @@ class Calculator extends Component {
     }
 
     _onInputButtonPressed(input) {
-        alert(input);
+        switch (typeof (input)) {
+            case 'number':
+                return this._handleNumberInput(input);
+        }
+    }
+
+    _handleNumberInput(num) {
+        let inputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+            inputValue: inputValue
+        });
     }
 }
 
--- src/Style.js
+++ src/Style.js
@@ -4,10 +4,20 @@ var Style = StyleSheet.create({
     rootContainer: {
         flex: 1
     },
+
     displayContainer: {
         flex: 2,
-        backgroundColor: '#193441'
+        backgroundColor: '#193441',
+        justifyContent: 'center'
     },
+    displayText: {
+        color: 'white',
+        fontSize: 38,
+        fontWeight: 'bold',
+        textAlign: 'right',
+        padding: 20
+    },
+
     inputContainer: {
         flex: 8,
         backgroundColor: '#3E606F'
