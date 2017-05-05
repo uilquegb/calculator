e530299b0942973e6d699b25d39c9fdbaeacb731
Uilque Messias <uilquegb@hotmail.com>
2017-05-05 18:10:30 -0300

    Show all buttons on input containter

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text, AppRegistry } from 'react-native';
+
 import Style from './Style';
+import InputButton from './InputButton';
+
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
 
 class Calculator extends Component {
 
@@ -8,9 +17,32 @@ class Calculator extends Component {
         return (
             <View style={Style.rootContainer}>
                 <View style={Style.displayContainer}></View>
-                <View style={Style.inputContainer}></View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
             </View>
         )
+    }
+
+    _renderInputButtons() {
+        let views = [];
+
+        for (var r = 0; r < inputButtons.length; r++) {
+            let row = inputButtons[r];
+
+            let inputRow = [];
+            for (var i = 0; i < row.length; i++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton value={input} key={r + '-' + i} />
+                );
+            }
+
+            views.push(<View style={Style.inputRow} key={'row-' + r}>{inputRow}</View>)
+        }
+
+        return views;
     }
 }
 
