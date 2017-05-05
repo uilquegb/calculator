fbb08d6882cb47b53183cdb141328033af7a9924
Uilque Messias <uilquegb@hotmail.com>
2017-05-05 15:39:39 -0300

    Restructure js files

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,1 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  AppRegistry
-} from 'react-native';
-
-class Calculator extends Component {
-
-  render() {
-    return (
-      <Text>Hello, React!</Text>
-    )
-  }
-}
-
-AppRegistry.registerComponent('Calculator', () => Calculator);
+import Calculator from './src/Calculator';
---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+class Calculator extends Component {
+
+  render() {
+    return (
+      <Text>Hello, React!</Text>
+    )
+  }
+}
+
+AppRegistry.registerComponent('Calculator', () => Calculator);
